9ed2e0231ce4a227a25dcd7d37a3b67161fc9f83
Vladislav Medvedev <medvedev_vv@bw-sw.com>
2017-11-16 10:32:34 +0700

    KFRD-6 Environment variable CONTAINERS was renamed to KAFKA_CONTAINERS

--- jenkins/clean.sh
+++ jenkins/clean.sh
@@ -2,7 +2,7 @@
 
 KAFKA_CONTAINERS=$(docker ps -a | grep "spotify/kafka" | awk '{ print $1 }')
 
-echo "Kafka containers: '$CONTAINERS'"
+echo "Kafka containers: '$KAFKA_CONTAINERS'"
 
 docker stop $KAFKA_CONTAINERS
 
